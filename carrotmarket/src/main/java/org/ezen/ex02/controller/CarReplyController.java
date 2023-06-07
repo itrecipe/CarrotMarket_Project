47143ba7b9dff3acc9e924a100cfa55c4fd79c47
itrecipe <itrecipe@naver.com>
2023-06-07 13:51:31 +0900

    spring_proejct
    23.6.7

---
+++ carrotmarket/src/main/java/org/ezen/ex02/controller/CarReplyController.java
@@ -0,0 +1,16 @@
+package org.ezen.ex02.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@RequestMapping("/replies") // replies라는 이름의 경로로 매핑
+@RestController //REST 방식을 사용한다.
+@Log4j
+@AllArgsConstructor
+public class CarReplyController {
+
+	
+}
